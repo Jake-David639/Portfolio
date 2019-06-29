7114bf741270d309dd18989d5fc00d2747813cbf
Jake David <jakedgmu13@gmail.com>
2019-06-29 10:22:18 -0400

    refactored boostrap portfolio for express

---
+++ server.js
@@ -0,0 +1,28 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+
+// Express settings to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// setting epress to use the public folder
+app.use(express.static('public'));
+
+// Routes
+// =============================================================
+
+// home route that renders the porfolio page
+app.get("/",(req, res) => {
+  res.render("index");
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(3000, () => {
+  console.log("App listening on PORT " + 3000);
+});
